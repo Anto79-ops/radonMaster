06f79b03c178dfa46dbdd5705422ae5b19980a4b
firstof9@gmail.com <firstof9@gmail.com>
2023-11-02 10:52:02 -0700

    another fix for suggested display precision

--- pubScribe.py
+++ pubScribe.py
@@ -161,6 +161,7 @@ def ha_discovery(serial: str = "00000000"):
         mqtt_data["unit_of_measurement"] = MQTT_SENSORS[sensor]["unit_of_measurement"]
         mqtt_data["value_template"] = MQTT_SENSORS[sensor]["value_template"]
         mqtt_data["state_topic"] = MQTT_SENSORS[sensor]["state_topic"]
+        mqtt_data["suggested_display_precision"] = None
         if "suggested_display_precision" in MQTT_SENSORS[sensor]:
             mqtt_data["suggested_display_precision"] = MQTT_SENSORS[sensor]["suggested_display_precision"]
 
